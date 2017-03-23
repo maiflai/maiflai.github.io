cb0efd76aca5ab0160fdaa9a6e53d1c03d36b925
Stu <maiflai@nospam>
2017-03-23 20:49:33 +0000

    clone from existing site

---
+++ photos/display.php
@@ -0,0 +1,46 @@
+<html><!-- #BeginTemplate "/Templates/main.dwt" --><!-- DW6 -->
+<head>
+<title>The Dovecote - a beautiful Listed Grade II* building converted for luxury romantic holidays and vactions in Oxfordshire</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="Description" content="Planning a romantic weekend in the heart of England? Stay at the Dovecote, Oxford in luxury.">
+<meta name="Keywords" content="dovecote,romantic,honeymoon,rural,retreat,oxford,cotswolds,vacation,england">
+<link rel="stylesheet" href="../stylesheets/screen.css" />
+</head>
+<body>
+<div style="text-align:left"><!-- #BeginLibraryItem "/Library/globalMenu.lbi" --><div id="globalMenu">
+<a href="../welcome.html">|&nbsp;welcome&nbsp;|</a>
+<a href="../history/history.html">|&nbsp;history&nbsp;|</a>
+<a href="../today/today.html">|&nbsp;today&nbsp;|</a>
+<a href="../features/features.html">| features&nbsp;|</a>
+<a href="../activities/activities.html">|&nbsp;activities&nbsp;|</a>
+<a href="index.php">|&nbsp;photos&nbsp;|</a>
+<a href="../contact.html">|&nbsp;contact&nbsp;|</a>
+</div><!-- #EndLibraryItem --></div> 
+<div id="mainBody">
+  <!-- #BeginEditable "content" -->
+  <div class="content">
+    <div class="colContainer" style="min-height:230px;">
+<div class="localMenu">
+  <div class="menuItem"><a class="menuItem" href="renovation.html">Renovation</a></div>
+  <div class="menuItem"><a class="menuItem" href="today.html">Today</a></div>
+</div>
+<?php $show = $HTTP_GET_VARS['toDisplay']; if(isSet($show)) print "<img style='width:100%;' src='" . rawurldecode($show) ."'>";?>
+ </div>
+</div>
+  <!-- #EndEditable -->
+</div>
+<div id="footer"><div style="width:100px;text-align:center"><img src="../images/dove_logo.gif" width="100" height="135"> 
+  <div style="margin-left:auto;margin-right:auto;"><a href="../about.html">sa272 &copy;2009</a></div></div></div>
+<!-- #BeginLibraryItem "/Library/frame.lbi" --><!-- #BeginLibraryItem "/Library/pictureMenu.lbi" --><div id="pictureMenu"> 
+<table>
+<tr>
+<td><img src="../images/menu/blank.png"></td><td><img src="../images/menu/blank.png"></td></tr>
+<tr>
+<td><img src="../images/menu/blank.png"></td><td><img src="../images/menu/blank.png"></td></tr>
+</table></div><!-- #EndLibraryItem --> 
+<div class="frameBorder" style="position:absolute;z-index:0;top:68px;right:0px;width:100%;height:2px;background-image:url(../images/spacer.gif);"><img src="../images/2spacer.gif"/></div>
+<div class="frameBorder" style="position:absolute;z-index:0;top:0px;right:68px;height:100%;width:2px;background-image:url(../images/spacer.gif);"><img src="../images/2spacer.gif"/></div>
+<img src="../images/window.gif" style="position:absolute;z-index:0;right:3px;top:3px;" width="132" height="132"> 
+<!-- #EndLibraryItem --> 
+</body>
+<!-- #EndTemplate --></html>
